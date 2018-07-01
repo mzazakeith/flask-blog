ca3920b75fc471942c7cfb7d2d4bcc448934512b
mzazakeith <mzazakeith@gmail.com>
2018-07-01 21:07:12 +0300

    all extensions initialised

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,11 @@ from flask_mail import Mail
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+# Instances of flask extensions
+# Instance of LoginManger and using its methods
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
