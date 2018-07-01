aa837ef2f662527a6f840e8085ee2ccc5efb104a
mzazakeith <mzazakeith@gmail.com>
2018-07-01 21:10:14 +0300

    register route done

--- app/auth/views.py
+++ app/auth/views.py
@@ -29,3 +29,15 @@ def logout():
     logout_user()
     return redirect(url_for("main.index"))
 
+
+@auth.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', registration_form=form)
+
