dc3b2314a6694bf99db696d204ee9063dc3c30bf
mzazakeith <mzazakeith@gmail.com>
2018-07-01 22:00:00 +0300

    shell and manager command for tests done

--- manage.py
+++ manage.py
@@ -24,3 +24,20 @@ manager.add_command('server', Server)
 manager.add_command('db', MigrateCommand)
 
 
+@manager.command
+def test():
+    """
+    Run the unit tests
+    """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Post=Post)
+
+
+if __name__ == '__main__':
+    manager.run()
