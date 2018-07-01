0e2b39672a5020f98c555e855970948095597366
mzazakeith <mzazakeith@gmail.com>
2018-07-01 21:59:00 +0300

    app instance created

--- manage.py
+++ manage.py
@@ -3,3 +3,8 @@ from app.models import User, Post
 # Set up migrations
 from flask_migrate import Migrate, MigrateCommand
 
+# Creating app instance
+# app = create_app('test')
+app = create_app('development')
+# app = create_app('production')
+
