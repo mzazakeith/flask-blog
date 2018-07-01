0a0393628caaba4cc045c58ce8a35b23490a9303
mzazakeith <mzazakeith@gmail.com>
2018-07-01 21:06:52 +0300

    importations for init.py

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
