8d0063482c47ed5970b6d28713d485d4a7e62767
mzazakeith <mzazakeith@gmail.com>
2018-07-01 21:07:32 +0300

    blue prints registered

--- app/__init__.py
+++ app/__init__.py
@@ -13,3 +13,10 @@ login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 mail = Mail()
+    # Regestering the main blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # Regestering the auth bluprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/')
