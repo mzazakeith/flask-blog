45f36b442ad2304a6e95d00f354ee004ebed4427
mzazakeith <mzazakeith@gmail.com>
2018-07-01 21:03:26 +0300

    form for main route

--- app/main/views.py
+++ app/main/views.py
@@ -18,3 +18,10 @@ def index():
     title = 'Home'
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=4)
+
+    form = SubscribeForm()
+    if form.validate_on_submit():
+        email = Subscription(email=form.email.data)
+        db.session.add(email)
+        db.session.commit()
+    return render_template('index.html', title=title, posts=posts, form=form)
