834919e78ec98c0f854fbb6f2fb511d17f61ff6c
mzazakeith <mzazakeith@gmail.com>
2018-07-01 21:11:57 +0300

    update account form done

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,3 +6,25 @@ from wtforms import ValidationError, PasswordField
 from wtforms import StringField, SubmitField, TextAreaField
 
 from app.models import User, Subscription
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[Length(min=2, max=20)])
+
+    email = StringField('Your Email Address', validators=[Email()])
+
+    picture = FileField('Change Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+
+    submit = SubmitField('Update information')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
