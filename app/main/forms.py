b9d91239b73e6c60c4dfdb5c968af94f1153a1c3
mzazakeith <mzazakeith@gmail.com>
2018-07-01 21:12:20 +0300

    post form done

--- app/main/forms.py
+++ app/main/forms.py
@@ -28,3 +28,9 @@ class UpdateAccountForm(FlaskForm):
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    content = TextAreaField('Content', validators=[Required()])
+    submit = SubmitField('Post')
