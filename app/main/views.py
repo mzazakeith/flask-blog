436860b5c9db4dc38baa33b675545486d5aead07
mzazakeith <mzazakeith@gmail.com>
2018-07-01 21:05:18 +0300

    account route made

--- app/main/views.py
+++ app/main/views.py
@@ -42,3 +42,20 @@ def save_picture(form_picture):
     image_file = url_for('static', filename='profile/' + current_user.image_file)
 
 
+@main.route("/user", methods=['GET', 'POST'])
+@login_required
+def profile():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        return redirect(url_for('main.profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile/' + current_user.image_file)
+    return render_template('profile/profile.html', title='Account', image_file=image_file, form=form)
