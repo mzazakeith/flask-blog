f11cacb4c7a9b2a92f74d06850017b185a27ea1d
mzazakeith <mzazakeith@gmail.com>
2018-07-01 21:06:28 +0300

    deleting function for comments and posts and also the update functions added

--- app/main/views.py
+++ app/main/views.py
@@ -9,6 +9,7 @@ from app.models import User, Post, Subscription, Comments
 from manage import app
 from . import main
 
+
 @main.route('/', methods=['GET', 'POST'])
 def index():
     """
@@ -25,6 +26,8 @@ def index():
         db.session.add(email)
         db.session.commit()
     return render_template('index.html', title=title, posts=posts, form=form)
+
+
 @main.route('/about')
 def about():
     title = 'About Us'
@@ -59,6 +62,8 @@ def profile():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile/' + current_user.image_file)
     return render_template('profile/profile.html', title='Account', image_file=image_file, form=form)
+
+
 @main.route("/post/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
@@ -77,7 +82,62 @@ def new_post():
 
         return redirect(url_for('main.index'))
     return render_template('create_post.html', title='New Post', form=form, legend='New Post')
+
+
+@main.route("/post/<int:post_id>", methods=['GET', 'POST'])
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = form.comment.data
+        new_comment = Comments(comment=comment, post_id=post_id, user=current_user)
+        new_comment.save_comments()
+        return redirect(url_for('main.post', post_id=post.id))
+    comments = Comments.query.filter_by(post_id=post_id).all()
+    return render_template('post.html', title=post.title, post=post, comment_form=form, comments=comments)
+
+
+@main.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if current_user.username != 'Keith Mzaza':
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        return redirect(url_for('main.post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form, post=post, legend='Update Post')
+
+
+@main.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if current_user.username != 'Keith Mzaza':
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('main.index'))
+
+
 @main.route("/comment/<int:comment_id>", methods=['GET', 'POST'])
 def comment(comment_id):
     comment = Comments.query.get_or_404(comment_id)
     return render_template('comment.html', title='Comment', comment=comment)
+
+
+@main.route("/comment/<int:comment_id>/delete", methods=['POST', 'GET'])
+@login_required
+def delete_comment(comment_id):
+    comment = Comments.query.get_or_404(comment_id)
+    if current_user.username != 'Keith Mzaza':
+        abort(403)
+    db.session.delete(comment)
+    db.session.commit()
+    return redirect(url_for('main.index'))
